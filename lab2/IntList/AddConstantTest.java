995a9979a2c75c8ef43313df2d61a24a68456f60
Jonathan <jhu4305@gmail.com>
2025-09-12 07:13:25 -0700

    add some cases

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -25,4 +25,32 @@ public class AddConstantTest {
         IntListExercises.addConstant(lst, 10);
         assertEquals("11 -> 12 -> 13 -> 14 -> 15 -> 16 -> 17 -> 18 -> 19", lst.toString());
     }
+
+    @Test
+    public void testAddConstantWithSingleElement() {
+        IntList lst = IntList.of(5);
+        IntListExercises.addConstant(lst, 3);
+        assertEquals("8", lst.toString());
+    }
+
+    @Test
+    public void testAddConstantWithNegativeValue() {
+        IntList lst = IntList.of(10, 20, 30);
+        IntListExercises.addConstant(lst, -5);
+        assertEquals("5 -> 15 -> 25", lst.toString());
+    }
+
+    @Test
+    public void testAddConstantWithZero() {
+        IntList lst = IntList.of(1, 2, 3);
+        IntListExercises.addConstant(lst, 0);
+        assertEquals("1 -> 2 -> 3", lst.toString());
+    }
+
+    @Test
+    public void testAddConstantWithNullList() {
+        IntList lst = null;
+        IntListExercises.addConstant(lst, 5);
+        assertNull(lst);
+    }
 }
